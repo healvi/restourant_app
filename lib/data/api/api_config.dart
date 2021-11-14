a77ed040d49798bd65259e757596ec9a25012164
healvi <57001337+healvi@users.noreply.github.com>
2021-11-14 15:56:10 +0700

    start sub 2

---
+++ lib/data/api/api_config.dart
@@ -0,0 +1,8 @@
+class ApiConfig {
+  int id = 0;
+  String query = '';
+  static const String baseUrll = 'https://restaurant-api.dicoding.dev/';
+  static const String list = 'list';
+  static const String detail = '/detail';
+  static const String search = '/search?=';
+}
